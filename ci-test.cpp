e99dc4ea14403626f7b7aef38c8808758d2904a4
Wipe <wipe@fonline2238.net>
2015-11-16 15:48:13 +0100

    .

--- ci-test.cpp
+++ ci-test.cpp
@@ -1,6 +1,19 @@
+#include <algorithm>
 #include <cstdlib>
+#include <iostream>
+#include <vector>
 
 int main( int argc, char** argv )
 {
+	std::vector<int> v = { 1, 2, 0, 7 };
+
+	std::cout << "1337 = ";
+	for_each( v.begin(), v.end(),
+	[]( const int& number )
+	{
+		std::cout << number;
+	});
+	std::cout << std::endl;
+
 	return( EXIT_SUCCESS );
 }
